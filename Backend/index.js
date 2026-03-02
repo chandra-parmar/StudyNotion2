7c7dece0e1a528b7a701e3422d05548bcf5f8bbb
chandra parmar <chandra.parmar2004@gmail.com>
2026-03-02 23:00:34 +0530

    middleware and route mounting

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const dbConnect= require('./config/database')
 const app = express()
+const port=5002
+const cookieParser = require('cookie-parser')
 
-const port=5000
 
-
+// Database connectivity
 dbConnect()
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser())
+
+
+//Routes mounting
+const authRoutes = require('./routes/authRoutes')
+app.use('/api/',authRoutes)
+
 
 app.listen(port,()=>{
     console.log("server is listing at port",port)
