7163d48534e2cd027c0e5fcb31855b16d6d69045
chandra parmar <chandra.parmar2004@gmail.com>
2026-03-04 12:02:32 +0530

    course field modify to array

--- Backend/models/Category.js
+++ Backend/models/Category.js
@@ -9,10 +9,10 @@ const CategorySchema = new mongoose.Schema(
       description:{
         type:String
       },
-      course:{
+      course:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Course'
-      }
+      }]
 })
 
 
