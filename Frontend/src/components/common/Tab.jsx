0f5248edb989127d0f6a869f349c17d4ab0da738
chandra parmar <chandra.parmar2004@gmail.com>
2026-07-24 23:18:36 +0530

    tab component added

---
+++ Frontend/src/components/common/Tab.jsx
@@ -0,0 +1,24 @@
+
+
+
+function Tab({ tabData , field, setField }){
+    return(
+        <div style={{ boxShadow : "inset 0px -1px 0px rgba(255,255,255,0.18)"}} className="flex bg-richblack-800 p-1 mb-6 text-sm font-medium gap-x-1 rounded-full max-w-max">
+          {
+            tabData.map((tab)=> (
+                <button
+                 key={tab.id}
+                 onClick={()=> setField(tab.type)}
+                 className={`${
+                  field === tab.type ? "bg-richblack-900 text-white" 
+                  : "bg-transparent text-richblack-200"} py-2 px-5 rounded-full transition-all duration-200`}>
+                  {tab.tabName}
+                  </button>
+            ))
+          }
+
+        </div>
+    )
+}
+
+export default Tab
