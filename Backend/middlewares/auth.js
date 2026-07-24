52694e454ca9c2da2e3f150ff124354ab97524f0
chandra parmar <chandra.parmar2004@gmail.com>
2026-07-24 21:48:37 +0530

    optional chaining for token extraction

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const auth = async(req,res,next)=>{
     try{
 
-        const token = req.cookies.token || req.body.token ||  req.header("Authorization")?.replace('Bearer ',"")
+        const token = req.cookies?.token || req.body?.token ||  req.header("Authorization")?.replace('Bearer ',"")
 
         // validation
         if(!token)
