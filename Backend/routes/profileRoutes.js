c1b14d6ded1fc0f4893458eea70041da42879961
chandra parmar <chandra.parmar2004@gmail.com>
2026-07-24 21:47:46 +0530

    get enrolled route added

--- Backend/routes/profileRoutes.js
+++ Backend/routes/profileRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const {updateProfile, deleteAccount, getUserDetails, updateDisplayPicture} = require("../controllers/profileController")
+const {updateProfile, deleteAccount, getUserDetails, updateDisplayPicture, getEnrolledCourses} = require("../controllers/profileController")
 const {auth} = require('../middlewares/auth')
 
 
-router.put('/:userId',auth,updateProfile)
+router.get('/enrolledCourses', auth, getEnrolledCourses)
 
-router.delete('/:id',auth,deleteAccount)
+router.put('/updateDisplayPicture/:id', auth, updateDisplayPicture)
 
-router.get('/:id',getUserDetails)
+router.put('/:userId', auth, updateProfile)
 
-router.put('/updateDisplayPicture/:id',auth,updateDisplayPicture)
+router.delete('/:id', auth, deleteAccount)
 
-
+router.get('/:id', getUserDetails)
 
 module.exports = router
