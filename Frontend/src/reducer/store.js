d062acd58a11820e6073ae17e97dee4918bd7448
chandra parmar <chandra.parmar2004@gmail.com>
2026-08-01 14:06:44 +0530

    course slice added

--- Frontend/src/reducer/store.js
+++ Frontend/src/reducer/store.js
@@ -3,12 +3,15 @@ import { configureStore} from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import profileReducer from './slices/profileSlice'
 import cartReducer from './slices/cartSlice'
+import courseReduer from './slices/courseSlice'
+
 
 export const  store = configureStore({
 
     reducer:{
         auth : authReducer,
         profile : profileReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        course : courseReduer
     }
 })
