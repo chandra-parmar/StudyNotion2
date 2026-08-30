da03c534334294861e5b5adeeb18ca9381217235
chandra parmar <chandra.parmar2004@gmail.com>
2026-08-30 15:23:23 +0530

    section subsection edit course api endpoint added

--- Frontend/src/services/apiEndpoints.js
+++ Frontend/src/services/apiEndpoints.js
@@ -18,21 +18,29 @@ export const profileEndpoints ={
 export const courseEndpoints = {
   GET_ALL_COURSE_API: BASE_URL + "/course/getAllCourses",
   COURSE_DETAILS_API: BASE_URL + "/course/getCourseDetails",
-  EDIT_COURSE_API: BASE_URL + "/course/editCourse",
+  EDIT_COURSE_API:  "http://localhost:4000/api/v1/course/",
   COURSE_CATEGORIES_API:  "http://localhost:4000/api/v1/category/",
-  CREATE_COURSE_API: BASE_URL + "/course/createCourse",
+  CREATE_COURSE_API:  "http://localhost:4000/api/v1/course/",
 
   
-  CREATE_SECTION_API: BASE_URL + "/course/addSection",
-  CREATE_SUBSECTION_API: BASE_URL + "/course/addSubSection",
-  UPDATE_SECTION_API: BASE_URL + "/course/updateSection",
-  UPDATE_SUBSECTION_API: BASE_URL + "/course/updateSubSection",
-  GET_ALL_INSTRUCTOR_COURSES_API: BASE_URL + "/course/getInstructorCourses",
-  DELETE_SECTION_API: BASE_URL + "/course/deleteSection",
-  DELETE_SUBSECTION_API: BASE_URL + "/course/deleteSubSection",
-  DELETE_COURSE_API: BASE_URL + "/course/deleteCourse",
-  GET_FULL_COURSE_DETAILS_AUTHENTICATED:
-    BASE_URL + "/course/getFullCourseDetails",
-  LECTURE_COMPLETION_API: BASE_URL + "/course/updateCourseProgress",
-  CREATE_RATING_API: BASE_URL + "/course/createRating",
+ 
+}
+
+
+//section endpoints
+export const sectionEndpoints ={
+
+   CREATE_SECTION_API:  "http://localhost:4000/api/v1/section/" ,
+   UPDATE_SECTION_API : "http://localhost:4000/api/v1/section/",
+   DELETE_SECTION_API : "http://localhost:4000/api/v1/section/"
+
+
+}
+
+//subsection api endpoints
+export const subSectionEndpoints ={
+
+    CREATE_SUB_SECTION_API :"http://localhost:4000/api/v1/subSection/",
+    UPDATE_SUB_SECTION_API: "http://localhost:4000/api/v1/subSection/",
+    DELETE_SUB_SECTION_API : "http://localhost:4000/api/v1/subSection/"
 }
