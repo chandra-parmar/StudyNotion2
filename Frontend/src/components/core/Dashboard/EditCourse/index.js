c58265764e0f71fd43de765ce0c533cd550110ff
chandra parmar <chandra.parmar2004@gmail.com>
2026-09-04 19:35:17 +0530

    edit course page added

---
+++ Frontend/src/components/core/Dashboard/EditCourse/index.js
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useParams} from 'react-router-dom'
+import { setCourse, setEditCourse} from '../../../../reducer/slices/courseSlice'
+
+
+export default function EditCourse (){
+
+    const dispatch = useDispatch()
+    const {courseId} = useParams()
+    const {course} = useSelector((state)=> state.course)
+    const [loading, setLoading] = useState(false)
+    const { token} = useSelector((state)=> state.auth)
+
+     useEffect(()=>{
+        const populateCourseDetails = async()=>{
+            setLoading(true)
+            const result = await getFullDetailsOfCourse(courseId, token)
+
+            if(result?.courseDetails)
+            {
+                dispatch(setEditCourse(true))
+                dispatch(setCourse(result?.courseDetails))
+            }
+            setLoading(false)
+        }
+     })
+    if(loading)
+    {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+
+
+    return(
+        <div className="text-white">
+            <h1>Edit course</h1>
+            <div>
+                {
+                    course ? (<RenderSteps></RenderSteps>) : (<p>Course not found</p>)
+                }
+            </div>
+        </div>
+    )
+}
