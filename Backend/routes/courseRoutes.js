486a83a94156298781604efcefa08da8f0f9a60e
chandra parmar <chandra.parmar2004@gmail.com>
2026-09-04 19:16:16 +0530

    delete course getinstructor course route added

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 
 const router = express.Router()
-const {createCourse,showAllCourses, getCourseDetails, editCourse} = require('../controllers/courseController')
+const {createCourse,showAllCourses, getCourseDetails, editCourse, getInstructorCourses, getFullCourseDetails,
+    deleteCourse
+} = require('../controllers/courseController')
 const {auth,isInstructor} = require('../middlewares/auth')
 
 
@@ -14,8 +16,19 @@ router.get('/',auth,showAllCourses)
 //get course details by id 
 router.get('/:courseId',getCourseDetails)
 
+//get full course detals
+router.get('/getFullCourse',auth,getFullCourseDetails)
+
 //edit course
 router.put('/',auth, isInstructor,editCourse)
+
+// get instructor course route
+router.get('/getInstructorCourses',auth,isInstructor,getInstructorCourses)
+
+
+//delete course
+router.delete('/',auth,isInstructor,deleteCourse)
+
 
 
 module.exports= router
