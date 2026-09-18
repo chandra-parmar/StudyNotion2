632798a68115b0a9abda852b35acf65e7eca0392
chandra parmar <chandra.parmar2004@gmail.com>
2026-09-18 20:54:45 +0530

    react restrict mode remove

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -11,7 +11,7 @@ import { Toaster} from 'react-hot-toast'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  
 
     <Provider store={ store}>
         <BrowserRouter>
@@ -20,5 +20,5 @@ root.render(
         </BrowserRouter>
     
     </Provider>
-  </React.StrictMode>
+  
 );
